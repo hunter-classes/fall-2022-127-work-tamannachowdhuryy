bbdeb74bf7683245c20e6274f1fdbcc135eef373
Tamanna chowdhury <104877190+tamannachowdhuryy@users.noreply.github.com>
2022-08-31 01:52:21 +0000

    added chapter-2

--- chapter-2/sent.py
+++ chapter-2/sent.py
@@ -8,6 +8,7 @@ word6 = "makes"
 word7 = "Jack"
 word8 = "a"
 word9 = "dull"
-word10 = "boy."
+word10 = "boy"
+word11 = "."
 
-print(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10)
+print(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10, word11)
