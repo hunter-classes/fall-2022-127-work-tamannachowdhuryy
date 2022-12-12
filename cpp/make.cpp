cd629ba93eba8c209906c0027143d9ac28b4f8ed
tamannachowdhuryy <104877190+tamannachowdhuryy@users.noreply.github.com>
2022-12-12 11:49:05 -0500

    Add files via upload

---
+++ cpp/make.cpp
@@ -0,0 +1,35 @@
+//DONE EXTRA: IF STATEMENT, LOOP AND WORKING FUNCTION 
+#include <iostream>
+#include <cmath> 
+
+//absolute value 
+int absolute_value(int v)
+{
+    return v * ((v > 0) - (v < 0));
+}
+
+//basic project
+int main() {
+  std::cout << "Hello World!\n" << std::endl;
+  int v = -45;
+  std::cout << "absolute value = " << absolute_value(v) << '\n';
+  
+ std::cout <<"-----------------------------------------\n";  
+// if statements to find two numbers is greater
+  int x = 20;
+  int y = 32;
+  if (x == y)
+    std::cout << "both are equal"; else if (x > y)
+    std::cout << x << " is greater than " << y << '\n';
+  else
+    std::cout << y << " is greater than " << x << '\n';
+   //loop starts 
+    
+  std::cout <<"-----------------------------------------\n";
+  for (int i = 0; i < 10; i ++) {
+        std::cout << "The square root of " << i << " is " << sqrt(double(i)) << "\n";
+    
+    }
+    
+  return 0;
+}
